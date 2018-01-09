f5c0df973daed5c8130b69682d6117f2a763cd32
Vinh Huynh <vinhhuynh115@gmail.com>
2018-01-09 23:56:39 +0700

    Add xcode proj

---
+++ circleci-example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  circleci-example
+//
+//  Created by Vinh Huynh on 1/9/18.
+//  Copyright © 2018 Vinh Huynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ circleci-example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  circleci-example
+//
+//  Created by Vinh Huynh on 1/9/18.
+//  Copyright © 2018 Vinh Huynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
